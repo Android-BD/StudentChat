36ade211b4b79d3f73d996ff3106e5e2802d42fe
Jhon Salguero <svjchrysler@gmail.com>
2016-11-22 21:35:32 -0400

    first commit

---
+++ app/src/main/java/com/seef/chat/student/studentchat/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.seef.chat.student.studentchat.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.seef.chat.student.studentchat.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/seef/chat/student/studentchat/models/Chat.java
@@ -0,0 +1,44 @@
+package com.seef.chat.student.studentchat.models;
+
+public class Chat {
+    private String username;
+    private String message;
+    private String photo;
+    private String idUser;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    @SuppressWarnings("unused")
+    public Chat() {
+    }
+}
